d68ce44f2f962e21f5492c92cdac3de1d3111db6
salmosstore <salmos.store7@gmail.com>
2026-09-17 20:20:32 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='salmos-pwa-v17';
-const CORE=['/','/index.html','/styles.css?v=17.0','/app.js?v=17.0','/config.js','/icon-192.png','/icon-512.png','/logo-mark.png','/banner-salmos-header.png','/manifest.webmanifest'];
+const CACHE='salmos-pwa-v18';
+const CORE=['/','/index.html','/styles.css?v=18.0','/app.js?v=18.0','/config.js','/icon-192.png','/icon-512.png','/logo-mark.png','/banner-salmos-header.png','/manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
